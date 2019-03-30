8ff363bb084d77a2283f71ec7129ab032721e2a4
snntaylan <sinan.taylan@outlook.com>
2019-03-31 00:02:45 +0100

    add a function for extract response data

--- mean-project/src/app/app.module.ts
+++ mean-project/src/app/app.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { FormsModule } from '@angular/forms';
-import { FormsModule } from '@angular/forms';
-
-
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
